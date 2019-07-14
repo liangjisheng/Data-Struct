45d83c5a921c356208ee35e50c5bfb5e6f275018
liangjisheng <1294851990@qq.com>
2019-07-14 18:06:26 +0800

    Algorithmic Graphics

---
+++ books/algorithmicGraphics/chapter3/countdown.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def countdown(i):
+    # base case
+    if i <= 0:
+        return 0
+    # recursive case
+    else:
+        print(i)
+        return countdown(i-1)
+
+countdown(3)
---
+++ books/algorithmicGraphics/chapter3/fact.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def fact(x):
+  if x == 1:
+    return 1
+  else:
+    return x * fact(x-1)
+
+print(fact(5))
